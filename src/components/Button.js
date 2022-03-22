4051cefbb0a4eb3b1d88378b93ce135230dbd065
Vince Emond <vince.emond@icloud.com>
2022-03-22 13:00:09 -0700

    Completed Button Setup

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,14 @@
 import React from "react";
+import classNames from "classnames";
 
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <button>{props.children}</button>;
+   const {confirm, danger, disabled, onClick} = props;
+   const buttonClass = classNames('button', {
+      'button--confirm': confirm, 
+      'button--danger': danger
+   });
+
+   return <button onClick={onClick} disabled={disabled} className={buttonClass}>{props.children}</button>;
 }
