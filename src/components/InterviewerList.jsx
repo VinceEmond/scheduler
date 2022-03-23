def603281b9548704fbf676859f9e556e6c709bd
Vince Emond <vince.emond@icloud.com>
2022-03-22 20:02:32 -0700

    Built the interviewers list

---
+++ src/components/InterviewerList.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import "components/InterviewerList.scss";
+import InterviewerListItem from './InterviewerListItem';
+
+const InterviewerList = (props) => {
+  const {interviewers, setInterviewer, interviewer} = props;
+
+  const interviewListItemsArr = interviewers.map((anInterviewer) => {
+    const {id, name, avatar} = anInterviewer;
+    return (
+      <InterviewerListItem
+        key={ id }
+        id={ id }
+        name={ name }
+        avatar={avatar}
+        selected={ id === interviewer }
+        setInterviewer={ setInterviewer }
+      />);
+  });
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{ interviewListItemsArr }</ul>
+    </section>
+  );
+};
+
+export default InterviewerList;
