38f635c7e0dd1acac590b5e41dc398dab7905b8d
Vince Emond <vince.emond@icloud.com>
2022-03-22 19:20:58 -0700

    Setup interviewerlistitem using storybook

---
+++ src/components/InterviewerListItem.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import "components/InterviewerListItem.scss";
+import classNames from 'classnames';
+
+const InterviewerListItem = (props) => { 
+  const {id, name, avatar , selected, setInterviewer} = props;
+
+  const interviewerItemClass = classNames(
+    'interviewers__item', {
+    'interviewers__item--selected': selected
+  });
+
+  return(
+    <li className={ interviewerItemClass } onClick={() => setInterviewer(id)}>
+      <img
+        className="interviewers__item-image"
+        src={avatar}
+        alt={name}
+      />
+      { selected ? name : ""}
+    </li>
+  );
+};
+
+export default InterviewerListItem;
