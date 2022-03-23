37e0450c87c0985035eb9e3b9e7448da0df0023d
Vince Emond <vince.emond@icloud.com>
2022-03-23 16:28:49 -0700

    Implemented confirm component and story

---
+++ src/components/Appointment/Confirm.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Button from 'components/Button';
+
+const Confirm = (props) => {
+  // const {message, onConfirm, onCancel} = props;
+
+  // message:String eg. "Delete the appointment?"
+  // onConfirm:Function to be called when the user clicks the Confirm button
+  // onCancel:Function to be called when the user clicks the Cancel button
+
+
+  return (
+    <main className="appointment__card appointment__card--confirm">
+      <h1 className="text--semi-bold">Delete the appointment?</h1>
+      <section className="appointment__actions">
+        <Button danger onClick={props.onCancel}>Cancel</Button>
+        <Button danger onClick={props.onConfirm}>Confirm</Button>
+      </section>
+    </main>
+  );
+}
+
+export default Confirm;
