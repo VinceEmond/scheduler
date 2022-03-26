1ddebe56d332e8ba79abf98375f62b32cf0fbf20
Vince Emond <vince.emond@icloud.com>
2022-03-25 17:18:02 -0700

    Built custom hook for useVisualMode

--- src/Hooks/useVisualMode.js
+++ src/Hooks/useVisualMode.js
@@ -1,16 +1,23 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
 const useVisualMode = (initialMode) => {
   const [mode, setMode] = useState(initialMode);
+  const [history, setHistory] = useState([initialMode]);
 
-  const transition = (newMode) => {
-    setMode(newMode)
+  const transition = (newMode, replace = false) => {
+    setHistory([newMode, ...history.slice((replace ? 1 : 0 ))])
+    setMode(newMode);
   }
 
-  return {mode, transition};
+  const back = () => {
+    if (history.length < 2) return;
+
+    const arrayWithItemRemoved = history.slice(1);
+    setHistory(arrayWithItemRemoved);
+    setMode(arrayWithItemRemoved[0]);
+  }
+
+  return {mode, transition, back};
 };
 
-const useCoolHook = (something) => {}
-
 export { useVisualMode };
-
