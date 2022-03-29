7fde619e4ec643dc91c0d29188ba0f0b8474e13b
Vince Emond <vince.emond@icloud.com>
2022-03-28 18:50:52 -0700

    Cleaned up the commented code which was moved to the hook

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -2,73 +2,11 @@ import React from "react";
 import DayList from "./DayList";
 import "components/Application.scss";
 import Appointment from "./Appointment";
-// import axios from "axios";
 import useApplicationData from "Hooks/useApplicationData";
 import {getAppointmentsForDay, getInterviewersForDay, getInterview} from "helpers/selectors";
 
 
 const Application = (props) => {
-
-  // const [state, setState] = useState({
-  //   day: "Monday",
-  //   days: [],
-  //   appointments: {},
-  //   interviewers: {}
-  // });
-
-  // const setDay = (day) => setState({...state, day});
-  
-  // useEffect(()=> {
-  //   const base = `http://localhost:8001/api`
-    
-  //   Promise.all([
-  //     axios.get(`${base}/days`), 
-  //     axios.get(`${base}/appointments`),
-  //     axios.get(`${base}/interviewers`)
-  //   ]).then((all)=> {
-  //     setState(prev => ({...prev,
-  //       days: all[0].data,
-  //       appointments: all[1].data,
-  //       interviewers: all[2].data
-  //     }))
-  //   })
-  // }, []);
-
-  // const cancelInterview = (id) => {
-
-  //   const appointment = {
-  //     ...state.appointments[id],
-  //     interview: null 
-  //   };
-
-  //   const appointments = {
-  //     ...state.appointments,
-  //     [id]: appointment
-  //   };
-
-  //   return axios.delete(`/api/appointments/${id}`)
-  //   .then(()=> {
-  //     setState({...state, appointments});
-  //   })
-  // }
-
-
-  // const bookInterview = (id, interview) => {
-  //   const appointment = {
-  //     ...state.appointments[id],
-  //     interview: { ...interview }
-  //   };
-
-  //   const appointments = {
-  //     ...state.appointments,
-  //     [id]: appointment
-  //   };
-
-  //   return axios.put(`/api/appointments/${id}`, {...appointment})
-  //   .then(() => {
-  //     setState({...state, appointments});
-  //   })
-  // }
 
   const {
     state,
